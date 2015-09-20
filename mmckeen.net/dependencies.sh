dddd03785ef4a1d8cd46ae0ec4ad6259154a2db6
Matthew McKeen <matthew@mmckeen.net>
2015-09-20 16:14:57 -0700

    Update to a openSUSE Tumbleweed base

--- mmckeen.net/dependencies.sh
+++ mmckeen.net/dependencies.sh
@@ -2,11 +2,11 @@
 # -----------
 #
 # The base OVF files needed to use the virtualbox-ovf builder can be downloaded as a tarball from 
-# http://susestudio.com/a/ZNpZV4/opensuse-13-1-packer-base
+# http://susestudio.com/
 #
 # Simply extract with tar xvf and the build template should have all the files it needs to build the Vagrant box
 # or, let this script do it for you!
 
-wget http://susestudio.com/download/26aad4f009e02de696d8409bf0dcafb9/openSUSE_13.1_Packer_Base.x86_64-1.0.0.ovf.tar.gz
+wget https://susestudio.com/download/6377290c9c96684ed97b67f4b8b14566/Matthews_openSUSE_Tumbleweed_JeOS.x86_64-0.0.2.ovf.tar.gz
 
 tar xvf *.tar.gz
